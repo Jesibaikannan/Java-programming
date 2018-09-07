af4a6d51d1a807fe16d4e72c64ca489dbb33c38d
Jesibaikannan <43060975+Jesibaikannan@users.noreply.github.com>
2018-09-07 13:42:28 +0530

    Add files via upload

---
+++ divi.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    int c;
+    int a=0;
+    int b=1;
+    c=a/b;
+    System.out.println(c);
+  }
+}
---
+++ float.java
@@ -0,0 +1,10 @@
+class Main {
+  public static void main(String[] args) {
+    float c;
+    int a=1;
+    float b=1.2f;
+    c=a*b;
+System.out.println(c);
+
+ }
+ }
