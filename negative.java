e9bcfafbaf5dc1d9a2ecfe3cd52bfc4bbee5c42b
Jesibaikannan <43060975+Jesibaikannan@users.noreply.github.com>
2018-10-04 12:30:55 +0530

    Update and rename positive.java to negative.java

---
+++ negative.java
@@ -0,0 +1,18 @@
+class Negative {
+  public static void main(String[] args) {
+    int a = 3;
+       if(a > 0)
+       {
+          System.out.println("positive");
+       }
+       else if(a < 0)
+       {
+          System.out.println("negative");
+       }
+       else
+       {
+          System.out.println("zero");
+      }
+    } 
+}
+
